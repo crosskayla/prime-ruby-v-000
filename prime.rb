c88374f2c7c39b84f304339f9e5fa8fefebc9445
Kayla Cross <kac288@georgetown.edu>
2018-02-26 00:37:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(number)
   return false if number < 2
-  range = (1..100).to_a
+  range = (2..100).to_a
   range.none? do |num|
     next if num == number
-    puts number % num 
+    puts number % num
     number % num == 0
   end
 end
